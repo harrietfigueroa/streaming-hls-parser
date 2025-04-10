a38caa95e71140ab9f9cf26fd791825a3a9982a5
Harriet F. Waters <harriet.f.waters@gmail.com>
2025-04-10 16:33:33 -0700

    more tests

--- src/hls/playlists/media-playlist.spec.ts
+++ src/hls/playlists/media-playlist.spec.ts
@@ -17,19 +17,16 @@ describe('Media Playlist', (): void => {
         });
     });
 
-    // describe('VOD Playlist', () => {
-    //     it('should parse a Media Playlist', async (): Promise<void> => {
-    //         const readable = loadTestFile(TestFiles.VOD_PLAYLIST)
-    //             .pipe(new NewlineTransformer())
-    //             .pipe(new HlsLexicalTransformer())
-    //             .pipe(new MediaPlaylistIngestTransformer());
+    describe('VOD Playlist', () => {
+        it('should parse a Media Playlist', async (): Promise<void> => {
+            const stream = loadTestFile(TestFiles.VOD_PLAYLIST);
+            const mediaPlaylist = await MediaPlaylist.fromStream(stream);
 
-    //         for await (const token of readable) {
-    //             console.dir(token);
-    //             expect(token).toHaveProperty('type');
-    //             expect(token.source).toBeDefined();
-    //             expect(token.value).toBeDefined();
-    //         }
-    //     });
-    // });
+            console.dir(mediaPlaylist);
+            expect(mediaPlaylist).toBeInstanceOf(MediaPlaylist);
+            expect(mediaPlaylist['#EXT-X-VERSION']).toBe(3);
+            expect(mediaPlaylist['#EXT-X-TARGETDURATION']).toBe(10);
+            expect(mediaPlaylist['#EXT-X-ENDLIST']).toBeTruthy();
+        });
+    });
 });
